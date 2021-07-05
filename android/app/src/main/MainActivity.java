6f4d1715438667270d1b97148ec0b634aa89f17a
Ferit Berkay Kabil <76204009+FeritBerkay@users.noreply.github.com>
2021-07-05 16:42:24 +0300

    Add files via upload

---
+++ android/app/src/main/MainActivity.java
@@ -0,0 +1,18 @@
+package co.appbrewery.xylophone;
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+@Override
++    public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+        new MethodChannel(flutterEngine.getDartExecutor().getBinaryMessenger(), CHANNEL)
+        .setMethodCallHandler(
+        (call, result) -> {
+        // Your existing code
+        }
+        );
+        }
+
